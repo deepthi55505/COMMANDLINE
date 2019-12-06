f9f4bc30d75b100b7abf037d76209c7e9a4321da
Deepthi4555 <58551963+Deepthi4555@users.noreply.github.com>
2019-12-06 14:56:54 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,33 @@
+var request=require('request');
+var readline = require('readline');
+var log = console.log;
+//let word;
+var options = { method : 'GET',
+     url: `https://fourtytwowords.herokuapp.com/word/advance/relatedWords?api_key=b972c7ca44dda72a5b482052b1f5e13470e01477f3fb97c85d5313b3c112627073481104fec2fb1a0cc9d84c2212474c0cbe7d8e59d7b95c7cb32a1133f778abd1857bf934ba06647fda4f59e878d164`
+}
+request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(response.body);
+  });
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let i=0;
+var recursiveAsyncReadLine = function () {
+    i++;
+     if(i==4){
+        return  rl.close();
+     }
+    rl.question('Word: ', function (word) {
+
+        if (word == 'advance'){
+            console.log("Correct guess")
+            return rl.close(); 
+        }
+        log("Wrong guess,Try again");
+        recursiveAsyncReadLine(); 
+    });
+};
+recursiveAsyncReadLine();
